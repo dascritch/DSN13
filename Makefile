b0e2bc806565d041996e82127aba6a3c919bce7b
dascritch <xavier@dascritch.com>
2014-05-19 18:59:44 +0200

    Import de l'existant

---
+++ Makefile
@@ -0,0 +1,4 @@
+#!/bin/bash
+scss -t compact source.scss compressed.css
+
+
---
+++ _define.php
@@ -0,0 +1,22 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+# This file is part of Ductile, a theme for Dotclear
+#
+# Copyright (c) 2011 - Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */				"Da Scritch 2013",
+	/* Description*/		"Da Scritch 2013 theme",
+	/* Author */			"dascritch.net d'après Ductile",
+	/* Version */			'3.0',
+	/* Properties */		array(
+								'standalone_config' => true
+							)
+);
+?>
